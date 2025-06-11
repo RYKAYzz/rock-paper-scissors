2f6c065d28fddf2132b261e3853bb0b28141b68b
Henry Kay <mireyhen@gmail.com>
2025-06-11 11:47:07 +0300

    Prompt user input set initial user and computer score 0

--- javascript.js
+++ javascript.js
@@ -6,47 +6,19 @@ function getComputerChoice() {
   let num = Math.floor(Math.random() * 3) + 1;
   if (num === 1) return "scissors";
   if (num === 2) return "paper";
-  else return "rock";
+  return "rock";
 }
+//Get humanChoice function
+//prompt for rock,paper or scissors
 
-//Write a function to ask for input
-//User inputs either rock,paper or scissors;
 function getHumanChoice() {
-  return prompt("Enter either rock,paper or scissors");
+  return prompt("Choose: rock,paper or scissors").toLocaleLowerCase();
 }
+//initialize variable to hold each score
+//Variable start value 0
 
-//Create variable to hold player scores
-//Initial value 0
 let humanScore = 0;
 let computerScore = 0;
 
-//play round 1
-//Write a function to get:
-//Get computerChoice
-//Get humanChoice
-function playRound(humanChoice, computerChoice) {
-  console.log("Human:", humanChoice);
-  console.log("Computer:", computerChoice);
-
-  if (humanChoice === computerChoice) return "That's a draw";
-
-  if (humanChoice === "rock" && computerChoice === "scissors")
-    return "You win, rock beats scissors";
-  if (humanChoice === "rock" && computerChoice === "paper")
-    return "You lose! Paper beats rock";
-
-  if (humanChoice === "scissors" && computerChoice === "paper")
-    return "You win, scissors beats paper";
-  if (humanChoice === "scissors" && computerChoice === "rock")
-    return "You lose! Rock beats scissors";
-
-  if (humanChoice === "paper" && computerChoice === "rock")
-    return "You win, paper beats rock";
-  if (humanChoice === "paper" && computerChoice === "scissors")
-    return "You lose! Scissors beats paper";
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+//Create round function
+//Hold two parameters for human and computer
